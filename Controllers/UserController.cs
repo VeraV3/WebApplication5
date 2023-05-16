3648694e2308652ea4c7df2bb66100c89e7338cd
VeraV3 <veravukosavljevic0@gmail.com>
2023-05-16 17:51:25 +0200

    Pokusala sam na drugi nacin da izvucem podatke iz tabele user, ali javlja se izuzetak koji kaze da user klasa nije dobro mapirana

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,48 +1,36 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebApplication5.Models;
 using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
 using NHibernate.Mapping.Attributes;
-using Npgsql;
-using WebApplication5.Models;
 
 namespace WebApplication5.Controllers
 {
-    [Controller]
-    public class UserController : ControllerBase
+    public class UserController : Controller
     {
-        private ISessionFactory CreateSessionFactory()
+        private static ISessionFactory CreateSessionFactory()
         {
-            var cfg = new Configuration();
-            cfg.DataBaseIntegration(db =>
+            var configuration = new Configuration();
+            configuration.DataBaseIntegration(x =>
             {
-                db.Dialect<PostgreSQL83Dialect>(); // Promenite na odgovarajući dialekt za vašu verziju PostgreSQL
-                db.Driver<NpgsqlDriver>(); // Promenite na odgovarajući drajver za PostgreSQL
-                db.ConnectionString = "Server=localhost;Port=5432;Database=mojabaza;User Id=postgres;Password=1234;"; // Prilagodite prema vašim informacijama o vezi
-                db.LogSqlInConsole = true;
+                x.ConnectionString = "Server=localhost;Port=5432;Database=mojabaza;User Id=postgres;Password=1234;";
+                x.Driver<NpgsqlDriver>();
+                x.Dialect<PostgreSQL82Dialect>();
             });
-
-            cfg.AddInputStream(HbmSerializer.Default.Serialize(typeof(User).Assembly));
-            var mapping = cfg.BuildMapping();
-            cfg.AddMapping((NHibernate.Cfg.MappingSchema.HbmMapping)mapping);
-
-            return cfg.BuildSessionFactory();
-
+            configuration.AddAssembly(typeof(User).Assembly);
+            return configuration.BuildSessionFactory();
         }
 
-        [HttpGet]
-        [Route("api/users")]
-        public ActionResult<IEnumerable<User>> GetUsers()
+        public ActionResult Index()
         {
-            var sessionFactory = CreateSessionFactory();
-
+            using (var sessionFactory = CreateSessionFactory())
             using (var session = sessionFactory.OpenSession())
             {
                 var users = session.Query<User>().Take(10).ToList();
-                return Ok(users);
+                return View(users);
             }
         }
     }
