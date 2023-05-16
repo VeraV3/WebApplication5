8ab4726802f7979ae29529048b3eed489be92846
VeraV3 <veravukosavljevic0@gmail.com>
2023-05-16 17:39:41 +0200

    napravila sam kontroler i pogled za model user, pokusala sam bez konfiguracijskog fajla da mapiram i konfigurisem NHibernate, i htela sam da prikazem korisnike iz postojece tabele, ali ne radi, greska 404

---
+++ Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Mapping.Attributes;
+using Npgsql;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    [Controller]
+    public class UserController : ControllerBase
+    {
+        private ISessionFactory CreateSessionFactory()
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(db =>
+            {
+                db.Dialect<PostgreSQL83Dialect>(); // Promenite na odgovarajući dialekt za vašu verziju PostgreSQL
+                db.Driver<NpgsqlDriver>(); // Promenite na odgovarajući drajver za PostgreSQL
+                db.ConnectionString = "Server=localhost;Port=5432;Database=mojabaza;User Id=postgres;Password=1234;"; // Prilagodite prema vašim informacijama o vezi
+                db.LogSqlInConsole = true;
+            });
+
+            cfg.AddInputStream(HbmSerializer.Default.Serialize(typeof(User).Assembly));
+            var mapping = cfg.BuildMapping();
+            cfg.AddMapping((NHibernate.Cfg.MappingSchema.HbmMapping)mapping);
+
+            return cfg.BuildSessionFactory();
+
+        }
+
+        [HttpGet]
+        [Route("api/users")]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var sessionFactory = CreateSessionFactory();
+
+            using (var session = sessionFactory.OpenSession())
+            {
+                var users = session.Query<User>().Take(10).ToList();
+                return Ok(users);
+            }
+        }
+    }
+}
